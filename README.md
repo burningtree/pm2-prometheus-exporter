7e01d0bb52d762d548e7d43c35ae776ec349513e
Jan Stránský <jan.stransky@arnal.cz>
2017-03-06 00:33:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# pm2-prometheus-exporter
+
+## Installation
+
+```shell
+pm2 install pm2-prometheus-exporter
+```
+
+## Author
+* Jan Stránský &lt;jan.stransky@arnal.cz&gt;
---
+++ exporter.js
@@ -0,0 +1,71 @@
+const http = require('http')
+const prom = require('prom-client')
+const pm2 = require('pm2')
+const argv = process.argv
+
+const prefix = 'pm2'
+const labels = [ 'name', 'instance' ]
+const map = [
+  [ 'up', 'Is the process running' ],
+  [ 'cpu', 'Process cpu usage' ],
+  [ 'memory', 'Process memory usage' ],
+  [ 'uptime', 'Process uptime' ],
+  [ 'instances', 'Process instances' ],
+  [ 'restarts', 'Process restarts' ],
+  [ 'loop_delay', 'V8 loop delay' ]
+]
+
+function pm2c (cmd, args = []) {
+  return new Promise((resolve, reject) => {
+    pm2[cmd](args, (err, resp) => {
+      if (err) return reject(err)
+      resolve(resp)
+    })
+  })
+}
+
+function metrics () {
+  let pm = {}
+  prom.register.clear()
+  map.forEach((m) => {
+    pm[m[0]] = new prom.Gauge(prefix + '_' + m[0], m[1], labels)
+  })
+  return pm2c('list').then((list) => {
+    list.forEach((p) => {
+      let conf = {
+        name: p.name,
+        instance: p.pm2_env.NODE_APP_INSTANCE
+      }
+      let loopDelay = p.pm2_env.axm_monitor['Loop delay']
+        ? p.pm2_env.axm_monitor['Loop delay'].value : false
+      let values = {
+        up: p.pm2_env.status === 'online' ? 1 : 0,
+        cpu: p.monit.cpu,
+        memory: p.monit.memory,
+        uptime: Math.round((Date.now() - p.pm2_env.pm_uptime) / 1000),
+        instances: p.pm2_env.instances || 1,
+        restarts: p.pm2_env.unstable_restarts,
+        loop_delay: loopDelay ? parseFloat(loopDelay.match(/^[\d.]+/)[0]) : null
+      }
+      Object.keys(values).forEach((k) => {
+        if (values[k] === null) return null
+        pm[k].set(conf, values[k])
+      })
+    })
+    return prom.register.metrics()
+  })
+}
+
+function exporter () {
+  const server = http.createServer((req, res) => {
+    metrics().then((data) => res.end(data))
+  })
+
+  const port = Number(argv[1]) || 9209
+
+  server.listen(port)
+  console.log('pm2-prometheus-exporter listening at %s', port)
+}
+
+exporter()
+
